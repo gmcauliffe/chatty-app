774097cee03f7ad80886640c7bb23756642e3e0d
Guy <guy.mcauliffe@icloud.com>
2018-09-04 20:41:36 +0000

    CurrentUser passed to Chatbar and added as placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
 import Chatbar from './Chatbar.jsx';
 import MessageList from './MessageList.jsx';
-/* import data from '../data.json'; */
+import data from '../data.json';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: {name: 'Franklin'},
+      messages: data
+    }
+  }
+
+
   render() {
     return (
       <div>
-        <MessageList />
-        <Chatbar /> 
+        <MessageList messages={ this.state.messages }/>
+        <Chatbar currentUser={ this.state.currentUser.name }/> 
       </div>
     );
   }
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
+// function UsernamePlaceholder({ currentUser }) {
+//   return (
+//     <input className="chatbar-username" placeholder={ currentUser } />
+//   )
+// }
+
 class Chatbar extends Component {
+
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder={ this.props.currentUser } />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
