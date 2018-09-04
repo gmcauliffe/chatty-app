6e8f0ec6660a258d8e60ab109f9a812f136bcb7d
Guy <guy.mcauliffe@icloud.com>
2018-09-04 19:29:53 +0000

    File structure set up and static HTML added to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import Chatbar from './Chatbar.jsx';
+import MessageList from './MessageList.jsx';
+/* import data from '../data.json'; */
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <body>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <Chatbar /> 
+      </body>
     );
   }
 }
