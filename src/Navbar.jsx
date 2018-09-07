f1358e27b7d2068d3513c6179737ed4af84d9303
Guy <guy.mcauliffe@icloud.com>
2018-09-07 00:16:49 +0000

    UserCount feature added and functional

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,6 +7,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
+          <span>{this.props.userCount}</span>
       </nav>
     );
   }
